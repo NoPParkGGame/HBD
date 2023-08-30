36fa6e52ee3a04dc662a9b5f40e153050e22ee51
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2023-08-30 14:15:50 +0000

    commit

---
+++ nightDayButton.js
@@ -0,0 +1,8 @@
+let coloring={
+    nightDay:function(self,coloring){
+        targetBody=document.querySelector('body')
+        if(self.value==="야간모드"){
+            targetBody.style.backgroundColor=coloring
+        }
+    }
+}
