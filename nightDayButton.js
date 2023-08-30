d7db9970148616b9d0a731b7f2673fed1a0285c7
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2023-08-30 14:58:04 +0000

    bordering

--- nightDayButton.js
+++ nightDayButton.js
@@ -1,16 +1,16 @@
 let coloring={
     nightDay:function(self,coloring1,coloring2){
-        [targetBody,targetAllBorder]=[document.querySelector('body'),document.querySelectorAll('border')]
+        [targetBody,targetAllBorder]=[document.querySelector('body'),document.querySelectorAll('.border')]
         if(self.value==="야간모드"){
             targetBody.style.backgroundColor=coloring1;
             targetBody.style.color=coloring2;
-            targetBody.style.borderColor=coloring2;
+            targetAllBorder.style.borderColor=coloring2;
             self.value="주간모드";
         }
         else{
             targetBody.style.backgroundColor=coloring2
             targetBody.style.color=coloring1;
-            targetBody.style.borderColor=coloring1;
+            targetAllBorder.style.borderColor=coloring1;
             self.value="야간모드"
         }
     }
