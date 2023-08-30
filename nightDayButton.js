7556247d1212f1d9db621cbea6e5cd1d0fb6b92d
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2023-08-30 14:32:27 +0000

    채ㅡㅑㅅ

--- nightDayButton.js
+++ nightDayButton.js
@@ -4,10 +4,12 @@ let coloring={
         if(self.value==="야간모드"){
             targetBody.style.backgroundColor=coloring1
             targetBody.style.color=coloring2
+            self.value='주간모드'
         }
         else{
             targetBody.style.backgroundColor=coloring2
             targetBody.style.color=coloring1
+            self.value="야간모드"
         }
     }
 }
