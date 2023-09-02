a972296c32e9e7944df03f1abbfe1f9dfe2b6131
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2023-09-02 14:34:57 +0000

    finally!!!!

--- nightDayButton.js
+++ nightDayButton.js
@@ -1,34 +1,30 @@
 let coloring={
     nightDay:function(self,coloring1,coloring2){
-        [targetBody]=[document.querySelector('body')]
-        let cssborders=['.page','#grid','#survey','.uni'];
-        let bordertest=document.querySelector('h1');
+        [targetBody,targetAllBorder]=[document.querySelector('body'),document.querySelectorAll('.border')]
         if(self.value==="야간모드"){
-            bordertest.style.borderColor=coloring2;
             targetBody.style.backgroundColor=coloring1;
             targetBody.style.color=coloring2;
-            self.value="주간모드";
-            /*
             let i=0;
             while(i<targetAllBorder.length){
                 targetAllBorder[i].style.borderColowr=coloring2;
                 i+=1
             }
-            */
+            self.value="주간모드";
             
         }
         else if(self.value==="주간모드"){
             targetBody.style.backgroundColor=coloring2;
-            targetBody.style.color=coloring1;
-            self.value='야간모드';
+            targetBody.style.color=coloring1;            
             targetAllBorder.style.borderColor=coloring1;
-            /*
             let i=0;
             while(i<targetAllBorder.length){
                 targetAllBorder[i].style.borderColor=coloring1;
                 i+=1
             }
-            */
+            self.value='야간모드';
+            
+
+            
         }
     }
 }
