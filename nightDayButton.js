9ffe5e80311b9dbf2dcb4187c2e1445a8ef3dde5
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2023-09-02 15:20:56 +0000

    Cval

--- nightDayButton.js
+++ nightDayButton.js
@@ -6,21 +6,12 @@ let coloring={
         if(self.value==="야간모드"){
             targetBody.style.backgroundColor=coloring1;
             targetBody.style.color=coloring2;
-            let i=0;
-        
-            while(i<borderList.length){
-                borderList[i].style.borderColor='white';
-            }
             self.value="주간모드";
         }
         else if(self.value==="주간모드"){
          
             targetBody.style.backgroundColor=coloring2;
-            targetBody.style.color=coloring1;
-            let i=0;
-            while(i<borderList.length){
-                borderList[i].style.borderColor=coloring1;
-            }         
+            targetBody.style.color=coloring1;      
             self.value='야간모드';
         }
     }
