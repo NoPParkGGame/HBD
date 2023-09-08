1d007b1d4ecfb0397c2c1f057425ee1329d5e88f
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2023-09-08 13:51:23 +0000

    commit

--- nightDayButton.js
+++ nightDayButton.js
@@ -11,15 +11,8 @@ let coloring={
             targetBody.style.color=coloring2;
             gridlist.style.backgroundColor=coloring1;
             
-            gridlist.style.cssText='border:3px black solid;
-            border-radius:20px;
-            display:grid;
-            grid-template-columns: 1fr 1fr auto;
-            background-color: black;
-            color:white;'
-            
-            /*change grid color*/
-            
+            gridlist.style.cssText='border:3px black solid; border-radius:20px; display:grid; grid-template-columns: 1fr 1fr auto; background-color: black; color:white;'
+                    
             self.value="주간모드";
         }
         else if(self.value==="주간모드"){
